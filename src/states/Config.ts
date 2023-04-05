cf4a55a5331f4396d39600888b6d60b33caca7ef
jiftechnify <jiftech.stlfy@gmail.com>
2023-04-06 01:38:33 +0900

    fix

--- src/states/Config.ts
+++ src/states/Config.ts
@@ -2,4 +2,4 @@ import { atom } from "jotai";
 
 export type PostDisplayMode = "normal" | "pubkey-hex-color";
 
-export const postDisplayModeAtom = atom<PostDisplayMode>("pubkey-hex-color");
+export const postDisplayModeAtom = atom<PostDisplayMode>("normal");
