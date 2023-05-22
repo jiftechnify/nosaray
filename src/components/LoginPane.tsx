55417f0c39241e90b638e88c4b5867def2d7ef19
jiftechnify <jiftech.stlfy@gmail.com>
2023-05-22 12:49:33 +0900

    refactor: LoginPane

--- src/components/LoginPane.tsx
+++ src/components/LoginPane.tsx
@@ -5,51 +5,35 @@ import {
   Button,
   Center,
   HStack,
-  Spinner,
   Text,
 } from "@chakra-ui/react";
 import { useAtomValue, useSetAtom } from "jotai";
-import { loadable } from "jotai/utils";
 import { nip07ExtAtom } from "../states/Nip07Ext";
 import { myPubkeyAtom } from "../states/Profiles";
 
 export const LoginPane: React.FC = () => {
-  const nip07Ext = useAtomValue(loadable(nip07ExtAtom));
+  const nip07Ext = useAtomValue(nip07ExtAtom);
   const setMyPubkey = useSetAtom(myPubkeyAtom);
 
   const handleClickLogin = async () => {
     setMyPubkey(await window.nostr.getPublicKey());
   };
 
-  const content = (() => {
-    switch (nip07Ext.state) {
-      case "loading":
-        return <Spinner />;
-
-      case "hasData":
-        const available = nip07Ext.data !== undefined;
-        return available ? (
-          <HStack>
-            <Text>NIP-07拡張機能で</Text>
-            <Button colorScheme="purple" onClick={handleClickLogin}>
-              ログイン
-            </Button>
-          </HStack>
-        ) : (
-          <Alert status="warning" w="max-content">
-            <AlertIcon />
-            <AlertTitle>NIP-07拡張機能が利用できません</AlertTitle>
-          </Alert>
-        );
-
-      case "hasError": // unreachable
-        return null;
-    }
-  })();
-
   return (
     <Center mt={4} height="54px">
-      {content}
+      {nip07Ext ? (
+        <HStack>
+          <Text>NIP-07拡張機能で</Text>
+          <Button colorScheme="purple" onClick={handleClickLogin}>
+            ログイン
+          </Button>
+        </HStack>
+      ) : (
+        <Alert status="warning" w="max-content">
+          <AlertIcon />
+          <AlertTitle>NIP-07拡張機能が利用できません</AlertTitle>
+        </Alert>
+      )}
     </Center>
   );
 };
